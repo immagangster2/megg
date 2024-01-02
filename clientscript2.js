ea82e035a9b978c319b1526a94d387906bd41a45
hahaha <132370261+immagangster2@users.noreply.github.com>
2024-01-02 18:09:51 +0100

    Add files via upload

---
+++ clientscript2.js
@@ -0,0 +1,51 @@
+(function () {
+  'use strict';
+
+  if (window.location.pathname == '/') {
+    var e = true;
+    class i {}
+    window.gd = new i();
+    if (typeof afykfdhgc != "undefined") {
+      if (afykfdhgc.guidetect() == "im here!") {
+        e = true;
+      }
+    } else {}
+    if (e == true) {
+      if (window.location.pathname == '/') {
+        function l(m) {
+          var q = document.getElementsByTagName("head")[0x0];
+          var r = document.createElement("style");
+          r.setAttribute('id', "customtheme");
+          r.type = "text/css";
+          r.appendChild(document.createTextNode(m));
+          q.appendChild(r);
+        }
+        document.body.insertAdjacentHTML("beforeend", "<div class=\"menu\">\n                          <i style=\"padding-left:13px;\">EOT Client Controls</i>\n                          <div class=\"settings\">\n                            <input id=\"clickMe\" type=\"button\" value=\"Lord\" onclick=\"('Lord Simulator Enabled'); eotc.lord();\" />\n                            <input id=\"clickMe\" type=\"button\" value=\"Bakkalci31\" onclick=\"('Bakkalci31 simulator Enabled'); eotc.bakkalci();\" />\n                            <input id=\"clickMe\" type=\"button\" value=\"Borucu\" onclick=\"('Borucu simulator Enabled'); eotc.borucu();\" />\n                            <input id=\"clickMe\" type=\"button\" value=\"Frosty\" onclick=\"('Frosty simulator Enabled'); eotc.frosty();\" />\n                            <input id=\"clickMe\" type=\"button\" value=\"Blank emoji\" onclick=\"('Blank emoji'); eotc.blank();\" />\n                            <input id=\"clickMe\" type=\"button\" value=\"AutoZoom\" onclick=\"('Autozoom Enabled'); eotc.autozoom();\" />\n                            <input id=\"clickMe\" type=\"button\" value=\"ResetZoom\" onclick=\"('Zoom Reset'); eotc.resetzoom();\" />\n                            <input id=\"clickMe\" type=\"button\" value=\"Ecp Checker\" onclick=\"('Checking...'); eotc.ecpcheck();\" />\n                            <input id=\"clickMe\" type=\"button\" value=\"Snipe\" onclick=\"('Snipe Enabled');\" />\n                            <input id=\"clickMe\" type=\"button\" value=\"MultiBox\" onclick=\"('MultiBox Enabled'); eotc.multibox(0,5);\" />\n                            <input id=\"clickMe\" type=\"button\" value=\"Become Random\" onclick=\"('Become Random');\" />\n                            <input id=\"clickMe\" type=\"button\" value=\"StealMod\" onclick=\"('Stealing'); eotc.stealmod();\" />\n                          </div>\n                        </div>");
+        l(".menu:not(:hover) .settings{display: none;} .menu:hover .settings{display: fixed; padding:4px;} .menu{position: fixed; top: 0; right: 0; z-index: 100000000; width: 160px; height: 20px; background-image: linear-gradient(160deg, hsl(230deg 93% 54% / 59%) 0, hsl(328deg 100% 60% / 60%) 100%); webkit-transition: .3s ease; transition: .3s ease;} .menu:hover{background-image: linear-gradient(160deg, hsl(230deg 93% 54% / 59%) 0, hsl(328deg 100% 60% / 60%) 100%); width: 160px; height: 280px;}");
+      }
+      if (localStorage.gui == undefined) {
+        localStorage.gui = 'on';
+      }
+      document.addEventListener("keydown", function (o) {
+        if (o.keyCode == 0xbd) {
+          if (localStorage.gui == 'on') {
+            document.getElementsByClassName("menu")[0x0].style.top = "-30px";
+            localStorage.gui = "off";
+            return;
+          }
+          if (localStorage.gui == "off") {
+            document.getElementsByClassName("menu")[0x0].style.top = "0px";
+            localStorage.gui = 'on';
+            return;
+          }
+        }
+      });
+      if (localStorage.gui == 'on') {
+        document.getElementsByClassName("menu")[0x0].style.top = "0px";
+      }
+      if (localStorage.gui == "off") {
+        document.getElementsByClassName("menu")[0x0].style.top = "-30px";
+      }
+    }
+  }
+})();
